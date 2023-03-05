e7eda22c56644772819871f428aafc7f0435ef1a
Rohan Kumar <96805095+rohankumar009@users.noreply.github.com>
2023-03-04 20:50:36 -0600

    Update Piece.java

--- Piece.java
+++ Piece.java
@@ -7,14 +7,7 @@ public class Piece {
     private int row;
     private int col;
     private boolean isBlack;
-
-    /**
-     * Constructor.
-     * @param character     The character representing the piece.
-     * @param row           The row on the board the piece occupies.
-     * @param col           The column on the board the piece occupies.
-     * @param isBlack       The color of the piece.
-     */
+    
     public Piece(char character, int row, int col, boolean isBlack) {
         this.character = character;
         this.row = row;
@@ -22,13 +15,6 @@ public class Piece {
         this.isBlack = isBlack;
     }
 
-    /**
-     * Determines if moving this piece is legal.
-     * @param board     The current state of the board.
-     * @param endRow    The destination row of the move.
-     * @param endCol    The destination column of the move.
-     * @return If the piece can legally move to the provided destination on the board.
-     */
     public boolean isMoveLegal(Board board, int endRow, int endCol) {
         switch (this.character) {
             // Pawn chars
@@ -66,53 +52,29 @@ public class Piece {
         }
     }
 
-    /**
-     * Sets the position of the piece.
-     * @param row   The row to move the piece to.
-     * @param col   The column to move the piece to.
-     */
     public void setPosition(int row, int col) {
         this.row = row;
         this.col = col;
     }
-
-    /**
-     * Returns the current chess unicode character.
-     * @return Unicode character.
-     */
+    
     public char getCharacter(){
         return this.character;
     }
 
-    /**
-     * Return the color of the piece.
-     * @return  The color of the piece.
-     */
     public boolean getIsBlack() {
         return this.isBlack;
     }
 
-    /**
-     * Tests the equality of two Piece objects based on
-     * their character parameter.
-     * @param other An instance of Piece to compare with this
-     *              instance.
-     * @return Boolean value representing equality result.
-     */
     public boolean equals(Piece other){
         return this.character == other.character;
     }
 
-    /**
-     * Returns a string representation of the piece.
-     * @return  A string representation of the piece.
-     */
     public String toString() {
         return "" + this.character;
     }
 
-
-    public void pawnPromo(char Character) { //Two big if statement with other if statement nested within the main two. One checks for the Black pieces moving to row 7 (or the White player end) and the other chekcs for the white pieces moving to row 0 (or the Black player's end)
+    // Pawn Promotion on chess board
+    public void pawnPromo(char Character) { 
         if (character == '\u265f') { // Checking for the Black Pawn to be on the side of the White Pieces
             if (row == 7){
                 System.out.println("Looks like your pawn can now be promoted, to choose- select K for Knight, Q for Queen, B for Bishop, and R for Rook");
@@ -169,5 +131,3 @@ public class Piece {
         }
     }
 }
-// If you do end up down here reading this comment then please go easy on the grading for my project 
-// I really hope this is correct and work, otherwise I think this should still count for most of the partial credit 
